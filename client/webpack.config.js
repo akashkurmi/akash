ddb0c2ea90283c80a4c2698557dc92f0e7c67c8a
Akash <kash@Akashs-MacBook-Pro.local>
2022-10-09 12:37:01 +0100

    docker server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,6 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-
-module.exports = async function(env, argv) {
-    const config = await createExpoWebpackConfigAsync({
-        ...env,
-        babel: {
-            dangerouslyAddModulePathsToTranspile: ['@ui-kitten/components']
-        }
-    }, argv);
-    return config;
-};
+module.exports = {
+    optimization:{
+        minimize: false, // <---- disables uglify.
+        // minimizer: [new UglifyJsPlugin()] if you want to customize it.
+    }
+}
